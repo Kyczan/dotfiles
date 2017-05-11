d0d44ee7740443e06257ab7aa0227ea1443f4a7a
Krystian Kowal <krystian@kowal.pro>
2017-05-11 22:53:28 +0200

    Change sound-output script to toggle outputs

--- i3/scripts/sound-output.sh
+++ i3/scripts/sound-output.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-pacmd set-default-sink $1
-pacmd list-sink-inputs | grep index | while read line
+SINK=$(pacmd list-sinks | grep -F '* index') 
+DEFSINK=$(echo $SINK | cut -f1 -d' ')
+echo $DEFSINK
+pacmd list-sinks | grep index | while read line
 do
-pacmd move-sink-input `echo $line | cut -f2 -d' '` $1
-
+  CURSINK=$(echo $line | cut -f2 -d' ')
+echo $CURSINK
+  if [[ $DEFSINK != $CURSINK ]]; then # not current sink so switch to it
+    pacmd set-default-sink $CURSINK
+    pacmd list-sink-inputs | grep index | while read line
+    do
+      pacmd move-sink-input `echo $line | cut -f2 -d' '` $CURSINK 
+    done
+  fi
 done
 
